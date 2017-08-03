eb445450294b4a4dd7e9afa8b444951ff7224352
Damian Polak <damianpolak.it@gmail.com>
2017-08-03 09:27:33 +0200

    .

--- bsize.sh
+++ bsize.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+echo "\r\nSMART MONITOR"
 date
+echo "\r\nSize of Directories"
 du -sh /var/www/html/uploads/
-du -sh /var/www/html/trash/
+du -sh /var/www/html/trashed/
 dcu=$(ls /var/www/html/uploads/ -lR | grep ^d | wc -l)
-dct=$(ls /var/www/html/trash/ -lR | grep ^d | wc -l)
+dct=$(ls /var/www/html/trashed/ -lR | grep ^d | wc -l)
+echo "\r\nDisk space"
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }'
+echo "\r\nFiles"
 echo "Uploads: " $dcu
-echo "Trash: " $dct
+echo "Trashed: " $dct
+echo "----"
+echo "\r\n"
